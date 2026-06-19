af5c01384624654771839725626cef80f40cf457
bayazidt <btnirob13@gmail.com>
2026-06-19 14:17:18 +0200

    user suggestions api endpoint changed

--- app/data/user.ts
+++ app/data/user.ts
@@ -3,7 +3,7 @@ import { User, UserList } from '../types/auth.types';
   
 
 export const getUser = async (): Promise<UserList> => {
-  return fetchAPI('http://localhost:8081/trbtree-service/api/v1/private/users?page=0&size=10');
+  return fetchAPI('http://localhost:8081/trbtree-service/api/v1/private/users/tree/60c8523c-23c7-4b7b-8a54-a9a2e69da5c4?page=0&size=10');
 };
 
 export const getUserById = async (id: string): Promise<User> => {
