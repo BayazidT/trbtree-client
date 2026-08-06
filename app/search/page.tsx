c1466c79bd0b0a308a4c6ba6b634e7efe0d7930d
bayazidt <btnirob13@gmail.com>
2026-08-06 23:10:56 +0200

    branch search implementation continued

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -90,7 +90,7 @@ export default function FeedPage() {
   let updatedLike=0;
     
   useEffect(() => {
-    getUserTree('0bcf6705-be5b-477b-aa44-b8c05e8d6ff2')
+    getFilterUsers(search)
       .then(setUser)
       .catch(console.error);
   }, []);
